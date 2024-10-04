96b589f250b70e977d1471b257b3cac527ff5ddb
Felipe Browne <153402996+bartowen@users.noreply.github.com>
2024-10-04 09:59:25 +0000

    outliers edad

--- src/utils.py
+++ src/utils.py
@@ -11,3 +11,10 @@ def db_connect():
     engine = create_engine(os.getenv('DATABASE_URL'))
     engine.connect()
     return engine
+
+def analisis_ric(columna,df):
+    estadisticas = df[columna].describe()
+    ric = estadisticas['75%'] - estadisticas['25%']
+    lim_sup = estadisticas['75%'] + 1.5*ric
+    lim_inf = estadisticas['25%'] - 1.5*ric 
+    print(f'superior {lim_sup} e inferior {lim_inf}')
